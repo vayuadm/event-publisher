b06995a7d3302114cb670c4163c111b717540a92
Sayar, Hezi <sayar@hpe.com>
2017-05-25 15:31:08 +0300

    rename to EventPublisher

--- src/main/java/com/hpe/ceribro/EventPublisher.java
+++ src/main/java/com/hpe/ceribro/EventPublisher.java
@@ -1,17 +1,17 @@
 package com.hpe.ceribro;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
-public class Scheduler {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    private static final Logger log = LoggerFactory.getLogger(Scheduler.class);
+@Component
+public class EventPublisher {
+
+    private static final Logger log = LoggerFactory.getLogger(EventPublisher.class);
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
